837cae0d64f4332c14342c02fd1fcf5ce6d6aeb2
naoki <74347818+C-naoki@users.noreply.github.com>
2022-01-18 04:02:03 +0900

    add "三連単" and "着順"

--- Predict.py
+++ Predict.py
@@ -63,8 +63,8 @@ if __name__ == '__main__':
     # 欲しい出馬表のrace_id, 日付を引数とする。
     # "https://db.netkeiba.com/race/" + race_id (データベース)
     # "https://race.netkeiba.com/race/shutuba.html?race_id=" + race_id (出馬表)
-    for venue in venue_id_list:
-        race_id_list = [venue + str(i).zfill(2) for i in range(1, 13)]
+    for venue_id in venue_id_list:
+        race_id_list = [venue_id + str(i).zfill(2) for i in range(1, 13)]
         # 出馬表のスクレイピング
         st = c.ShutubaTable.scrape(race_id_list=race_id_list, date=date)
         st.preprocessing()
@@ -90,10 +90,11 @@ if __name__ == '__main__':
             rt = c.Return(today_return_tables)
             tansho_df = rt.tansho
             fukusho_df = rt.fukusho
+            sanrentan_df = rt.sanrentan
             sanrenpuku_df = rt.sanrenpuku
             predict_df = pd.DataFrame(
                                         index = [], 
-                                        columns = ["本命馬ランク", "三連複ランク", "本命馬◎", "対抗馬○", "単穴馬▲", "連下馬1△", "連下馬2△", "連下馬3△", "本命馬着順", "単勝オッズ", "三連複結果", "三連複オッズ", "単勝回収金額", "三連複回収金額"]
+                                        columns = ["本命馬ランク", "三連複ランク", "本命馬◎", "対抗馬○", "単穴馬▲", "連下馬1△", "連下馬2△", "連下馬3△", "単勝オッズ", "三連単結果", "三連単オッズ", "三連複オッズ", "単勝回収金額", "三連単回収金額", "三連複回収金額"]
                                     )
         except:
             return_chk = 0
@@ -110,34 +111,44 @@ if __name__ == '__main__':
                 race_num = proba[0][-2:]
             race_proba = proba[1].sort_values('score', ascending = False).head(6)
             # 三連複のランクの決定
-            if race_proba.iat[min(5, len(race_proba)-1), 1] < 0:
-                triple_rank = "C"
-            elif race_proba.iat[min(5, len(race_proba)-1), 1] < 0.5:
-                triple_rank = "B"
-            else:
+            if len(race_proba["score"][race_proba["score"]>=1.5])>=3:
                 triple_rank = "A"
-            # 本命馬のランクの決定
-            if race_proba.iat[0, 1] - race_proba.iat[1, 1] < 0.4:
-                favorite_rank = "C"
-            elif race_proba.iat[0, 1] - race_proba.iat[1, 1] < 1:
-                favorite_rank = "B"
+            elif len(race_proba["score"][race_proba["score"]>=1.5])>=2:
+                triple_rank = "B"
+            elif len(race_proba["score"][race_proba["score"]>=1.5])>=1:
+                triple_rank = "C"
             else:
+                triple_rank = "-"
+            # 本命馬のランクの決定
+            if race_proba.iat[0, 1] - race_proba.iat[1, 1] >= 1 and race_proba.iat[0, 1] >= 3:
                 favorite_rank = "A"
+            elif race_proba.iat[0, 1] - race_proba.iat[1, 1] >= 1 or race_proba.iat[0, 1] >= 3:
+                favorite_rank = "B"
+            elif race_proba.iat[0, 1] - race_proba.iat[1, 1] >= 0.4:
+                favorite_rank = "C"
+            else:
+                favorite_rank = "-"
             # race_id_listの長さが6に満たない場合、"-"で埋める
             s = pd.Series(['-', '-'], index=race_proba.columns, name=race_proba.index[0])
             for j in range(6-len(race_proba)):
                 race_proba=race_proba.append(s)
             if return_chk:
                 # 本命馬の着順結果を取得
-                real_arrival = arrival_tables_df.loc[proba[0],:][arrival_tables_df.loc[proba[0],"馬番"]==race_proba.iat[0, 0]]["着順"].iat[0]
-                if real_arrival != "除": real_arrival = int(real_arrival)
+                real_arrival = list()
+                for j in range(6):
+                    if len(arrival_tables_df.loc[proba[0],:][arrival_tables_df.loc[proba[0],"馬番"]==race_proba.iat[j, 0]]["着順"]) == 1:
+                        real_arrival.append(str(arrival_tables_df.loc[proba[0],:][arrival_tables_df.loc[proba[0],"馬番"]==race_proba.iat[j, 0]]["着順"].iat[0]))
+                    else:
+                        real_arrival.append("-")
                 # 単勝オッズを取得
                 tansho_odds = tansho_df.loc[proba[0], "return"] / 100
                 # 単勝回収率の計算
-                if real_arrival == 1: tansho_money = tansho_df.loc[proba[0], "return"]
+                if int(real_arrival[0]) == 1: tansho_money = tansho_df.loc[proba[0], "return"]
                 else: tansho_money = 0
-                # 三連複の結果を取得
-                sanrenpuku_results = str(sanrenpuku_df.loc[proba[0], "win_0"])+" - "+str(sanrenpuku_df.loc[proba[0], "win_1"])+" - "+str(sanrenpuku_df.loc[proba[0], "win_2"])
+                # 三連単の結果を取得
+                sanrentan_results = str(sanrentan_df.loc[proba[0], "win_0"])+" → "+str(sanrentan_df.loc[proba[0], "win_1"])+" → "+str(sanrentan_df.loc[proba[0], "win_2"])
+                # 三連単のオッズを取得
+                sanrentan_odds = sanrentan_df.loc[proba[0], "return"] / 100
                 # 三連複のオッズを取得
                 sanrenpuku_odds = sanrenpuku_df.loc[proba[0], "return"] / 100
                 # 三連複が的中したかどうか記録
@@ -149,8 +160,11 @@ if __name__ == '__main__':
                 # 三連複の回収率の計算
                 if sanrenpuku_chk[i] == 3: sanrenpuku_money = sanrenpuku_df.loc[proba[0], "return"]
                 else: sanrenpuku_money = 0
+                # 三連単の回収率の計算
+                if sanrenpuku_chk[i] == 3 and real_arrival[0] == '1': sanrentan_money = sanrentan_df.loc[proba[0], "return"]
+                else: sanrentan_money = 0
                 # データをdfに追加
-                predict_df.loc[race_num+"R"] = [favorite_rank, triple_rank, race_proba.iat[0, 0], race_proba.iat[1, 0], race_proba.iat[2, 0], race_proba.iat[3, 0], race_proba.iat[4, 0], race_proba.iat[5, 0], real_arrival, tansho_odds, sanrenpuku_results, sanrenpuku_odds, tansho_money, sanrenpuku_money]
+                predict_df.loc[race_num+"R"] = [favorite_rank, triple_rank, str(race_proba.iat[0, 0])+" ("+real_arrival[0]+")", str(race_proba.iat[1, 0])+" ("+real_arrival[1]+")", str(race_proba.iat[2, 0])+" ("+real_arrival[2]+")", str(race_proba.iat[3, 0])+" ("+real_arrival[3]+")", str(race_proba.iat[4, 0])+" ("+real_arrival[4]+")", str(race_proba.iat[5, 0])+" ("+real_arrival[5]+")", tansho_odds, sanrentan_results, sanrentan_odds, sanrenpuku_odds, tansho_money, sanrentan_money, sanrenpuku_money]
             else:
                 # データをdfに追加
                 predict_df.loc[race_num+"R"] = [favorite_rank, triple_rank, race_proba.iat[0, 0], race_proba.iat[1, 0], race_proba.iat[2, 0], race_proba.iat[3, 0], race_proba.iat[4, 0], race_proba.iat[5, 0]]
@@ -174,25 +188,35 @@ if __name__ == '__main__':
                 max_length = 0
                 for i, cell in enumerate(col):
                     max_length = max(max_length, len(str(cell.value)))
-                    if cell.coordinate[0] == 'I': ws[cell.coordinate].border = border2
-                    else: ws[cell.coordinate].border = border1
+                    if cell.coordinate[0] == 'I' or cell.coordinate[0] == 'C': ws[cell.coordinate].border = border2 # 二重線の記入
+                    else: ws[cell.coordinate].border = border1 # 線の記入
                     # ランクA及び着順の予想が的中した時、セルをオレンジ色にする
-                    if ws[cell.coordinate].value == 'A' or (cell.coordinate[0] == 'J' and cell.value == 1) or (cell.coordinate[0] == 'L' and sanrenpuku_chk[i] == 3):
-                        ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='ffa500')
+                    if ws[cell.coordinate].value == 'A' or (cell.coordinate[0] == 'J' and ws[list(ws.columns)[3][i].coordinate].value[-3:] =="(1)") or (cell.coordinate[0] == 'M' and sanrenpuku_chk[i] == 3) or (cell.coordinate[0] == 'L' and ws[list(ws.columns)[3][i].coordinate].value[-3:] =="(1)" and sanrenpuku_chk[i] == 3):
+                        ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='ffbf7f')
                     # ランクB及び着順の予想が3着以内及び三連複の3頭中2頭的中した時、セルを水色にする
-                    if ws[cell.coordinate].value == 'B' or (cell.coordinate[0] == 'J' and (cell.value == 2 or cell.value == 3)) or (cell.coordinate[0] == 'L' and sanrenpuku_chk[i] == 2):
-                        ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='87ceeb')
-                    # ランクC及び及び三連複の3頭中1頭のみ的中した時、セルを水色にする
-                    if ws[cell.coordinate].value == 'C' or (cell.coordinate[0] == 'L' and sanrenpuku_chk[i] == 1):
+                    if ws[cell.coordinate].value == 'B' or (cell.coordinate[0] == 'J' and (ws[list(ws.columns)[3][i].coordinate].value[-3:]=="(2)" or ws[list(ws.columns)[3][i].coordinate].value[-3:]=="(3)")) or (cell.coordinate[0] == 'M' and sanrenpuku_chk[i] == 2) or (cell.coordinate[0] == 'L' and ws[list(ws.columns)[3][i].coordinate].value[-3:] =="(1)" and sanrenpuku_chk[i] == 2):
+                        ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='a8d3ff')
+                    # ランクC及び及び三連複の3頭中1頭のみ的中した時、セルを灰色にする
+                    if ws[cell.coordinate].value == 'C' or (cell.coordinate[0] == 'M' and sanrenpuku_chk[i] == 1) or (cell.coordinate[0] == 'L' and ws[list(ws.columns)[3][i].coordinate].value[-3:] =="(1)" and sanrenpuku_chk[i] == 1):
                         ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='d3d3d3')
                     if cell.coordinate[0] == 'A' or cell.coordinate[1:] == '1':
                         ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='000000')
                         ws[cell.coordinate].font = Font(color="ffffff")
+                    # 文字を中心に配置する
                     cell.alignment = Alignment(horizontal = 'center', 
                                         vertical = 'center',
                                         wrap_text = False)
                 adjusted_width = max_length * 2.08
                 ws.column_dimensions[col[0].column_letter].width = adjusted_width
+            for row in ws.rows:
+                if 1 < int(row[0].coordinate[1:]) < 14:
+                    for cell, score in zip(row[3:9], proba_table.loc[venue_id+str(int(row[0].coordinate[1:])-1).zfill(2)].sort_values('score', ascending = False)["score"].head(6)):
+                        if score > 3:
+                            ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='ffbf7f')
+                        elif score > 2:
+                            ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='a8d3ff')
+                        elif score > 1.5:
+                            ws[cell.coordinate].fill = PatternFill(patternType='solid', fgColor='d3d3d3')
             wb.save(excel_path)
         else:
             print("<"+venue_name+">")
