86e6e5bf4947096735bc7b27bd45f194b7ba5f0e
naoki <74347818+C-naoki@users.noreply.github.com>
2022-01-23 16:16:12 +0900

    Changes with the new race_id_list

--- code/Scrape.py
+++ code/Scrape.py
@@ -9,42 +9,46 @@ from environment.variables import *
 
 import pandas as pd
 
-race_id_list_2021 = []
+race_id_list_2022 = {}
 for place in range(1, 11, 1):
+    race_id_place = []
     for kai in range(1, 13, 1):
         for day in range(1, 13, 1):
             for r in range(1, 13, 1):
-                race_id = "2021" + str(place).zfill(2) + str(kai).zfill(2) + str(day).zfill(2) + str(r).zfill(2)
-                race_id_list_2021.append(race_id)
+                race_id = "2022" + str(place).zfill(2) + str(kai).zfill(2) + str(day).zfill(2) + str(r).zfill(2)
+                race_id_place.append(race_id)
+    race_id_list_2022[place] = race_id_place
 
-# # Resultsのスクレイピング
-# new_race_results = c.Results.scrape(race_id_list_2021,
-#                                     pre_race_results=_dat.race_results_2021
-#                                     )
-# print(new_race_results.shape)
-# print(_dat.race_results_2021.shape)
-# new_race_results.to_pickle("_dat/pickle/2021/race_results.pickle")
+# Resultsのスクレイピング
+new_race_results = c.Results.scrape(race_id_list_2022,
+                                    pre_race_results=_dat.race_results_2022
+                                    )
+print(new_race_results)
+new_race_results.to_pickle("../_dat/train_data/2022/race_results.pickle")
+horse_id_list_2022 = new_race_results['horse_id'].unique()
 
-# # Pedsのスクレイピング
-# new_ped_results = c.Peds.scrape(_dat.horse_id_list_2021,
-#                                 pre_ped_results=_dat.ped_results_2021
-#                                 )
-# print(new_ped_results)
-# # print(_dat.ped_results_2021.shape)
-# new_ped_results.to_pickle("_dat/pickle/2021/ped_results.pickle")
+# Pedsのスクレイピング
+new_ped_results = c.Peds.scrape(horse_id_list_2022,
+                                pre_ped_results=_dat.ped_results_2022
+                                )
+print(new_ped_results)
+new_ped_results.to_pickle("../_dat/train_data/2022/ped_results.pickle")
 
-# # horse_resultsのスクレイピング
-# new_horse_results = c.HorseResults.scrape(_dat.horse_id_list_2021,
-#                                         pre_horse_results=_dat.horse_results_2021
-#                                         )
-# print(new_horse_results.shape)
-# print(_dat.horse_results_2021.shape)
-# new_horse_results.to_pickle("_dat/pickle/2021/horse_results.pickle")
+# horse_resultsのスクレイピング
+new_horse_results = c.HorseResults.scrape(horse_id_list_2022,
+                                        pre_horse_results=_dat.horse_results_2022
+                                        )
+print(new_horse_results)
+new_horse_results.to_pickle("../_dat/train_data/2022/horse_results.pickle")
 
-# # return_tablesのスクレイピング
-# new_return_tables, _ = c.Return.scrape(race_id_list_2021,
-#                                     pre_return_tables=_dat.return_tables_2021
-#                                     )
-# print(new_return_tables.shape)
-# print(_dat.return_tables_2021.shape)
-# new_return_tables.to_pickle("_dat/pickle/2021/return_tables.pickle")
+# return_tablesのスクレイピング
+new_return_tables, _ = c.Return.scrape(race_id_list_2022,
+                                    pre_return_tables=_dat.return_tables_2022
+                                    )
+print(new_return_tables)
+new_return_tables.to_pickle("../_dat/train_data/2022/return_tables.pickle")
+
+m.update_data(_dat.race_results, _dat.race_results_2022).to_pickle("../_dat/train_data/overall/race_results.pickle")
+m.update_data(_dat.horse_results, _dat.horse_results_2022).to_pickle("../_dat/train_data/overall/horse_results.pickle")
+m.update_data(_dat.ped_results, _dat.ped_results_2022).to_pickle("../_dat/train_data/overall/ped_results.pickle")
+m.update_data(_dat.return_tables, _dat.return_tables_2022).to_pickle("../_dat/train_data/overall/return_tables.pickle")
