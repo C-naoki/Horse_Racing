0c9eb8ef5b27dc0c8a5c032631e9f0dbf29d95e4
naoki <74347818+C-naoki@users.noreply.github.com>
2022-07-08 15:21:40 +0900

    changes due to new classes

--- _dat/DataFrame.py
+++ _dat/DataFrame.py
@@ -3,46 +3,19 @@ horse_results = {}
 ped_results = {}
 race_results = {}
 return_tables = {}
+jockey_results = {}
+trainer_results = {}
+owner_results = {}
+breeder_results = {}
 horse_id_list = {}
 
-horse_results["overall"] = pd.read_pickle("../_dat/train_data/overall/horse_results.pickle")
-ped_results["overall"] = pd.read_pickle("../_dat/train_data/overall/ped_results.pickle")
-race_results["overall"] = pd.read_pickle("../_dat/train_data/overall/race_results.pickle")
-return_tables["overall"] = pd.read_pickle("../_dat/train_data/overall/return_tables.pickle")
-horse_id_list["overall"] = race_results["overall"]['horse_id'].unique()
-
-horse_results["2022"] = pd.read_pickle("../_dat/train_data/2022/horse_results.pickle")
-ped_results["2022"] = pd.read_pickle("../_dat/train_data/2022/ped_results.pickle")
-race_results["2022"] = pd.read_pickle("../_dat/train_data/2022/race_results.pickle")
-return_tables["2022"] = pd.read_pickle("../_dat/train_data/2022/return_tables.pickle")
-horse_id_list["2022"] = race_results["2022"]['horse_id'].unique()
-
-horse_results["2021"] = pd.read_pickle("../_dat/train_data/2021/horse_results.pickle")
-ped_results["2021"] = pd.read_pickle("../_dat/train_data/2021/ped_results.pickle")
-race_results["2021"] = pd.read_pickle("../_dat/train_data/2021/race_results.pickle")
-return_tables["2021"] = pd.read_pickle("../_dat/train_data/2021/return_tables.pickle")
-horse_id_list["2021"] = race_results["2021"]['horse_id'].unique()
-
-horse_results["2020"] = pd.read_pickle("../_dat/train_data/2020/horse_results.pickle")
-ped_results["2020"] = pd.read_pickle("../_dat/train_data/2020/ped_results.pickle")
-race_results["2020"] = pd.read_pickle("../_dat/train_data/2020/race_results.pickle")
-return_tables["2020"] = pd.read_pickle("../_dat/train_data/2020/return_tables.pickle")
-horse_id_list["2020"] = race_results["2020"]['horse_id'].unique()
-
-horse_results["2019"] = pd.read_pickle("../_dat/train_data/2019/horse_results.pickle")
-ped_results["2019"] = pd.read_pickle("../_dat/train_data/2019/ped_results.pickle")
-race_results["2019"] = pd.read_pickle("../_dat/train_data/2019/race_results.pickle")
-return_tables["2019"] = pd.read_pickle("../_dat/train_data/2019/return_tables.pickle")
-horse_id_list["2019"] = race_results["2019"]['horse_id'].unique()
-
-horse_results["2018"] = pd.read_pickle("../_dat/train_data/2018/horse_results.pickle")
-ped_results["2018"] = pd.read_pickle("../_dat/train_data/2018/ped_results.pickle")
-race_results["2018"] = pd.read_pickle("../_dat/train_data/2018/race_results.pickle")
-return_tables["2018"] = pd.read_pickle("../_dat/train_data/2018/return_tables.pickle")
-horse_id_list["2018"] = race_results["2018"]['horse_id'].unique()
-
-horse_results["2017"] = pd.read_pickle("../_dat/train_data/2017/horse_results.pickle")
-ped_results["2017"] = pd.read_pickle("../_dat/train_data/2017/ped_results.pickle")
-race_results["2017"] = pd.read_pickle("../_dat/train_data/2017/race_results.pickle")
-return_tables["2017"] = pd.read_pickle("../_dat/train_data/2017/return_tables.pickle")
-horse_id_list["2017"] = race_results["2017"]['horse_id'].unique()
+for key in ['overall', '2017', '2018', '2019', '2020', '2021', '2022']:
+    horse_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/horse_results.pickle")
+    ped_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/ped_results.pickle")
+    race_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/race_results.pickle")
+    return_tables[key] = pd.read_pickle("../_dat/train_data/"+key+"/return_tables.pickle")
+    jockey_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/jockey_results.pickle")
+    trainer_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/trainer_results.pickle")
+    owner_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/owner_results.pickle")
+    breeder_results[key] = pd.read_pickle("../_dat/train_data/"+key+"/breeder_results.pickle")
+    horse_id_list[key] = race_results[key]['horse_id'].unique()
